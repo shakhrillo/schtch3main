47404b1c27aec7cdd4eaa8490652a8d3f417c717
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-07-29 12:47:55 +0500

    fromDate

--- ui/sch2/app/features/items/items.py
+++ ui/sch2/app/features/items/items.py
@@ -50,7 +50,8 @@ async def get_items(params: GetItemsParams = Depends(GetItemsParams),
     toDate = params.to_date
 
     if toDate == fromDate:
-        toDate = datetime.strptime(toDate, "%Y-%m-%d") + timedelta(days=1)
+        toDate = datetime.strptime(fromDate, "%Y-%m-%d") + timedelta(days=1)
+
     if toDate == "":
         # add today
         toDate = datetime.now() + timedelta(days=1)
