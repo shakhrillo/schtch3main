9c7adf1c9ab8a9acec3423df727bef29ed311527
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-07-31 12:13:33 +0500

    useEffect

--- ui/sch2/src/components/BaseCreatetable/BaseCreatetable.js
+++ ui/sch2/src/components/BaseCreatetable/BaseCreatetable.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import moment from 'moment';
 import { Form, Alert, Table, Spinner, Modal } from 'react-bootstrap';
 import BaseFileUpload from '../BaseFileUpload/BaseFileUpload';
