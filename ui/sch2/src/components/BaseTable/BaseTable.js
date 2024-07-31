479acaecd49377f4d359e07552f0e813e5e21f18
Shakhrillo Tukhsanov <mr.jhon1327@gmail.com>
2024-07-31 17:40:57 +0500

    dd

--- ui/sch2/src/components/BaseTable/BaseTable.js
+++ ui/sch2/src/components/BaseTable/BaseTable.js
@@ -97,6 +97,20 @@ const BaseTable = ({
     setComments(com);
   };
 
+  const [images, setImages] = useState([]);
+  const [showImg, setShowImg] = useState(false);
+
+
+  const handleShowImg = (img) => {
+    setShowImg(true);
+    setImages(img);
+  };
+
+  const handleCloseImg = () => {
+    setShowImg(false);
+  };
+
+
   const dateDay = (date) => {
     if (moment(date).format('dddd') === 'Monday') return 'Mo';
     if (moment(date).format('dddd') === 'Tuesday') return 'Di';
@@ -422,7 +436,30 @@ const BaseTable = ({
                       ({item?.notes?.length})
                     </div>
                   </td>
-                  <td className="">
+                  <td>
+                    <div className="d-flex flex-row justify-content-center">
+                      <span
+                        onClick={() => handleShowImg(item?.image)}
+                        className={`cursor_pointer mx-2 text-${
+                          darkMode ? 'white' : 'dark'
+                        }`}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          fill="currentColor"
+                          className="bi bi-chat-text"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z" />
+                          <path d="M4 5.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8zm0 2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5z" />
+                        </svg>
+                      </span>
+                      ({item?.image?.length})
+                    </div>
+                  </td>
+                  {/* <td className="">
                     <div className="d-flex justify-content-center align-items-center">
                       {item?.image ? (
                         <a
@@ -447,13 +484,34 @@ const BaseTable = ({
                         '-'
                       )}
                     </div>
-                  </td>
+                  </td> */}
                 </tr>
               </Fragment>
             ) : null;
           })}
         </tbody>
       </Table>
+      <Modal show={showImg} onHide={handleCloseImg} scrollable={true} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>Bild</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="d-flex justify-content-center">
+            {images.map((img, i) => {
+              return (
+                <a key={img + i} href={url + img} target="_blank" rel="noreferrer" >
+                  Open image
+                </a>
+                )
+            })}
+          </div>
+        </Modal.Body>
+        <Modal.Footer className="d-flex justify-content-center">
+          <Button variant="secondary" onClick={handleCloseImg}>
+            Schließen
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <Modal show={show} onHide={handleClose} scrollable={true} size="lg">
         <Modal.Header closeButton>
           <Modal.Title>Notiz</Modal.Title>
